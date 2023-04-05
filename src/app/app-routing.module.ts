cdc2e23111288d42573fc5abeb47dfaaad66cb8e
sateesh gedela <sateeshgedela4219@gmail.com>
2023-04-05 15:33:32 +0530

    commit the home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path:'', component:EmployeeComponent}
+  // {path:'', component:EmployeeComponent},
+  {path:'', component:HomeComponent}
 ];
 
 @NgModule({
