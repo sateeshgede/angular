098d4f6715772f596e3374ee80eda13b56537074
sateesh gedela <sateeshgedela4219@gmail.com>
2023-04-05 13:25:42 +0530

    Create employee component and changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './c/footer/footer.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
